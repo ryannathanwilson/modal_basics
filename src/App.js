b2b757805ca9c92fdfc9e0b9c4a2694621b9f946
RNW <54713487+ryannathanwilson@users.noreply.github.com>
2021-01-26 10:33:30 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './css/global.css';
+
+function App() {
+  return (
+    <div className="RNW REACT">
+        <h1>
+			RNW REACT TEMPLATE
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+	  		Updated 11 January 2021
+        </a>
+    </div>
+  );
+}
+
+export default App;
